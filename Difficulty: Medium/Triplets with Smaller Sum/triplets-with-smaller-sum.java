fdf066ac69f3081ecb973c92ab67c28114ad8144
Sangeet Dalai <dalaisangeet@gmail.com>
2026-07-06 12:28:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Triplets with Smaller Sum/triplets-with-smaller-sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    int countTriplets(int sum, int arr[]) {
+        int n = arr.length;
+        Arrays.sort(arr);
+        int count = 0;
+
+        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                if (arr[i] + arr[left] + arr[right] < sum) {
+                    count += right - left;  
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return count;
+    }
+}
